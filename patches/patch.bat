28adb3ee70146dd0496e6cc3096be78982ea8994
MihailKovachev <md.kovachev@protonmail.com>
2025-01-30 14:56:19 +0200

    Added patches to prepare for quartz as a submodule

---
+++ patches/patch.bat
@@ -0,0 +1,13 @@
+copy ".\quartz.layout.ts" "..\quartz\quartz.layout.ts"
+copy ".\quartz.config.ts" "..\quartz\quartz.config.ts"
+
+copy ".\plugins\emitters\*" "..\quartz\quartz\plugins\emitters"
+copy ".\plugins\filters\*" "..\quartz\quartz\plugins\filters"
+copy ".\plugins\transformers\*" "..\quartz\quartz\plugins\transformers"
+copy ".\styles\*" "..\quartz\quartz\styles"
+
+copy ".\components\styles\*" "..\quartz\quartz\components\styles"
+copy ".\components\scripts\*" "..\quartz\quartz\components\scripts"
+copy ".\components\*.*" "..\quartz\quartz\components\"
+
+pause
