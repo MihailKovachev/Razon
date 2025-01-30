1a406c030b7f0740c13c974b812ff2e0f1427803
MihailKovachev <md.kovachev@protonmail.com>
2025-01-30 15:14:16 +0200

    Removed literals from patch script

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,11 +1,11 @@
-cp "./quartz.layout.ts" "../quartz/quartz.layout.ts"
-cp "./quartz.config.ts" "../quartz/quartz.config.ts"
+cp ./quartz.layout.ts ../quartz/quartz.layout.ts
+cp ./quartz.config.ts ../quartz/quartz.config.ts
 
-cp "./plugins/emitters/*" "../quartz/quartz/plugins/emitters/"
-cp "./plugins/filters/*" "../quartz/quartz/plugins/filters/"
-cp "./plugins/transformers/*" "../quartz/quartz/plugins/transformers/"
-cp "./styles/*" "../quartz/quartz/styles/"
+cp ./plugins/emitters/* ../quartz/quartz/plugins/emitters/
+cp ./plugins/filters/* ../quartz/quartz/plugins/filters/
+cp ./plugins/transformers/* ../quartz/quartz/plugins/transformers/
+cp ./styles/* ../quartz/quartz/styles/
 
-cp "./components/styles/*" "../quartz/quartz/components/styles/"
-cp "./components/scripts/*" "../quartz/quartz/components/scripts/"
-cp "./components/*.*" "../quartz/quartz/components/"
+cp ./components/styles/* ../quartz/quartz/components/styles/
+cp ./components/scripts/* ../quartz/quartz/components/scripts/
+cp ./components/*.* ../quartz/quartz/components/
